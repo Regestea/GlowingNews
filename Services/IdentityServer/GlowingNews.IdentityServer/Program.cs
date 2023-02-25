84ad98cb663e61baf09d7e829c496e446c8755ab
Regestea <amirhossein5starstar@outlook.com>
2023-02-25 14:43:22 +0330

    remove unused grpc

--- Services/IdentityServer/GlowingNews.IdentityServer/Program.cs
+++ Services/IdentityServer/GlowingNews.IdentityServer/Program.cs
@@ -96,7 +96,7 @@ builder.Services.AddAuthorization(options =>
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
 
 app.MapGrpcService<AuthorizationGrpcService>();
 // Configure the HTTP request pipeline.
