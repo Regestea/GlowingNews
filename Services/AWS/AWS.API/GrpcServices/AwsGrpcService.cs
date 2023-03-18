2d495546303f903d49e5923d8e06c5ae185ce4cc
Regestea <amirhossein5starstar@outlook.com>
2023-03-18 19:30:53 +0330

    aws full finished

--- Services/AWS/AWS.API/GrpcServices/AwsGrpcService.cs
+++ Services/AWS/AWS.API/GrpcServices/AwsGrpcService.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace AWS.API.GrpcServices
 {
-    public class AwsGrpcService: AwsService.AwsServiceBase
+    public class AwsGrpcService : AwsService.AwsServiceBase
     {
         private AwsIndexDbContext _awsIndexDbContext;
 
@@ -17,27 +17,28 @@ namespace AWS.API.GrpcServices
 
         public override async Task<GetObjectPathResponse> GetObjectPathByToken(GetObjectPathRequest request, ServerCallContext context)
         {
-           Guid userId=Guid.Parse(request.UserId);
-           var awsFile = await _awsIndexDbContext.AwsFiles.FirstOrDefaultAsync(x => x.UserId == userId && x.HaveUse == false && x.Token == request.Token);
-           if (awsFile!=null)
-           {
-               awsFile.HaveUse=true;
-               awsFile.Token = "";
-               _awsIndexDbContext.AwsFiles.Update(awsFile);
-               await _awsIndexDbContext.SaveChangesAsync();
-               return new GetObjectPathResponse() { FilePath = awsFile.FullPath };
-           }
+            Guid userId = Guid.Parse(request.UserId);
+            var awsFile = await _awsIndexDbContext.AwsFiles.FirstOrDefaultAsync(x => x.UserId == userId && x.HaveUse == false && x.Token == request.Token);
+            if (awsFile != null)
+            {
+                awsFile.HaveUse = true;
+                awsFile.Token = "";
+                _awsIndexDbContext.AwsFiles.Update(awsFile);
+                await _awsIndexDbContext.SaveChangesAsync();
+                return new GetObjectPathResponse() { FilePath = awsFile.FullPath };
+            }
 
-           return new GetObjectPathResponse() { FilePath = "" };
+            return new GetObjectPathResponse() { FilePath = "" };
         }
 
         public override async Task<DeleteObjectResponse> DeleteObjectByPath(DeleteObjectRequest request, ServerCallContext context)
         {
             Guid userId = Guid.Parse(request.UserId);
-            var awsFile = await _awsIndexDbContext.AwsFiles.FirstOrDefaultAsync(x=>x.FullPath==request.FilePath&&x.UserId==userId);
+            var path = request.FilePath.Split("/");
+            var awsFile = await _awsIndexDbContext.AwsFiles.FirstOrDefaultAsync(x => x.FileName == path[1] && x.UserId == userId);
             if (awsFile != null)
             {
-                awsFile.HaveUse=false;
+                awsFile.HaveUse = false;
                 _awsIndexDbContext.AwsFiles.Update(awsFile);
                 await _awsIndexDbContext.SaveChangesAsync();
             }
