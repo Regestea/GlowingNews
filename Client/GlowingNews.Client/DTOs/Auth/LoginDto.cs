8582a2fce7ef6a72077439dbe48ac4147db27934
regestea23 <amirhossein5starstar@outlook.com>
2023-04-16 17:51:02 +0330

    add loging and register page

---
+++ Client/GlowingNews.Client/DTOs/Auth/LoginDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GlowingNews.Client.DTOs.Auth
+{
+    public class LoginDto
+    {
+        [Required]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        public string Password { get; set; } = null!;
+    }
+}
